894a530a86c1a92cd46227c28d585c8543fea562
Simon Groenborg <groenborg27@gmail.com>
2017-01-31 14:52:32 +0100

    updated test script

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e -u -x
 
-cd git-phlow-repo/
+#RESOLVE OWN PROJECT AS DEPENDENCY
+buildpath=$pwd
+deppath= $GOPATH/src/github.com/praqma
 
-ls
+mkdir -p $deppath
+cp -R git-phlow/ $deppath
+
+# RESOLVE EXTERNAL DEPENDENCIES
+cd $deppath/git-phlow
 go get -d
 go get github.com/stretchr/testify
 go get github.com/smartystreets/goconvey
 
-pwd
-ls
-
+#cp binary to buildpath
 go test -v ./...
