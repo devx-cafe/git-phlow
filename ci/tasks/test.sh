1a3e44a8eeebf677f8ea783f91889789317f9885
Simon Groenborg <groenborg27@gmail.com>
2017-02-04 22:01:33 +0100

    close #14 Update concourse pipeline

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e -u -x
 
-#RESOLVE OWN PROJECT AS DEPENDENCY
+#MOVE PROJECT TO GOPATH
 buildpath=$(pwd)
 
 mkdir -p $GOPATH/src/github.com/praqma
 cp -R git-phlow/ $GOPATH/src/github.com/praqma
 
-# RESOLVE EXTERNAL DEPENDENCIES
+# RESOLVE DEPENDENCIES - TEST AND PRODUCTION
 cd $GOPATH/src/github.com/praqma/git-phlow
 go get -d -v ./...
 go get github.com/stretchr/testify
 go get github.com/smartystreets/goconvey
 go get github.com/spf13/cobra
 
-#cp binary to buildpath
+#RUN WHOLE TEST SUITE -VERBOSE
 go test -v ./...
