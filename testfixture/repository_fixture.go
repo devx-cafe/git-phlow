ae19bcac7101dbc46c6708b6a834509c96cfc44b
Simon Groenborg <groenborg27@gmail.com>
2017-02-09 12:30:26 +0100

    Added zip, ignore, and fixture

--- testfixture/repository_fixture.go
+++ testfixture/repository_fixture.go
@@ -1,23 +1,90 @@
 package main
 
-import "fmt"
 import (
-	"os/user"
 	"os"
+	"archive/zip"
+	"path/filepath"
+	"io"
+	"fmt"
+	"errors"
 )
 
-var ProjectDirectory string = os.Getenv("GOPATH") + "/src/github.com/praqma/git-phlow"
+const (
+	target   string = "./"
+	archive  string = "./testfixture/phlow-test-pkg.zip"
+	testPath string = ""
+)
 
-func InitializeEmptyRepository() {
+var (
+	GoPathNotSet error = errors.New("GOPATH is empty")
+	goPath       string
+)
 
-	currentUser, _ := user.Current()
+//init
+//Runs before functions to setup variable gopath
+func init() {
+	goPath = os.Getenv("GOPATH")
+	if len(goPath) == 0 {
+		fmt.Fprintln(os.Stdout, GoPathNotSet)
+		os.Exit(1)
+	}
+}
 
-	usr, _ := user.Lookup(currentUser.Name)
+//unzip
+//unzips archive to target directory
+func unzip(archive, target string) error {
+	reader, err := zip.OpenReader(archive)
+	if err != nil {
+		return err
+	}
 
-	fmt.Fprintln(os.Stdout, usr.HomeDir)
+	if err := os.MkdirAll(target, 0755); err != nil {
+		return err
+	}
+
+	for _, file := range reader.File {
+		path := filepath.Join(target, file.Name)
+		if file.FileInfo().IsDir() {
+			os.MkdirAll(path, file.Mode())
+			continue
+		}
+
+		fileReader, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer fileReader.Close()
+
+		targetFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+		if err != nil {
+			return err
+		}
+		defer targetFile.Close()
+
+		if _, err := io.Copy(targetFile, fileReader); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+//SetupTestRepo
+func SetupTestRepo() {
+	err := unzip(archive, target)
+
+	if err != nil {
+		fmt.Fprintln(os.Stdout, err.Error())
+		os.Exit(1)
+	}
+	fmt.Fprintln(os.Stdout, "Local test repository created from 'zip'")
+}
+
+//TearDownTestRepo
+func TearDownTestRepo() {
 
 }
 
 func main() {
-	InitializeEmptyRepository()
+	SetupTestRepo()
 }
