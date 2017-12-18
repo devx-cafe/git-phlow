faeed63a218d44ee45c428d1397290776a76c3f6
Simon Grønborg <groenborg27@gmail.com>
2017-12-18 22:15:16 +0100

    close #294 update pipeline test for new windows worker

--- ci/chocolatey/chocolatey.ps1
+++ ci/chocolatey/chocolatey.ps1
@@ -1,23 +1,29 @@
+## concourse task for releasing a package on chocolatey
 
-ls
+ls   #debugging
+
+# read version from file
 $version = Get-Content .\gp-version\version
 
 Expand-Archive .\phlow-artifact-windows-s3\git-phlow-*.zip -DestinationPath .\phlow-artifact-windows-s3
 
+# list artifact for debugging purpose
 ls .\phlow-artifact-windows-s3
+
 
 mv phlow-artifact-windows-s3/git-phlow.exe git-phlow/ci/chocolatey/tools/
 
 cd git-phlow/ci/chocolatey
 ls tools/
 
+# update the nuspec file
 $NuSpecFilePath = ".\git-phlow.nuspec"
 $file = Get-Item -Path $NuSpecFilePath
 [ xml ]$fileContents = Get-Content -Path $NuSpecFilePath
 $fileContents.package.metadata.version = '{0}' -f $version
 $fileContents.Save($file.Fullname)
 
-$token = [Environment]::GetEnvironmentVariable("chocotoken","User")
+$token = $env:chocotoken
 
 choco pack
 
--- ci/windows/go-test-windows.ps1
+++ ci/windows/go-test-windows.ps1
@@ -1,20 +1,26 @@
 #SET GOPATH TO TMP DIRECTORY, BECAUSE WE WANT THE DEPENDENCIES LOCALLY
+
+# this line is important. The worker have been configured through a build session, so the powershell
+# have not been reloaded to update the path. Therefore we must set it each time. 
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+
 $env:GOPATH = "$PWD\"
 $env:GOBIN = "$PWD\bin"
 
-#NAVIGATE TO FOLDER
-mkdir -p $env:GOPATH/src/github.com/praqma
+##NAVIGATE TO FOLDER
+mkdir -Path $env:GOPATH/src/github.com/praqma
 cp -R git-phlow/ $env:GOPATH/src/github.com/praqma
 
 cd $env:GOPATH/src/github.com/praqma/git-phlow
+ls
 
-#GET DEPENDENCIES
+##GET DEPENDENCIES
 go get -d -t -v ./...
 
-#RUN TESTS
+##RUN TESTS
 cd plugins
 go test -v ./...
 
-# USES THE LATEST EXITCODE, WHICH IS FROM GO TEST, AND EXITS WITH THAT. THIS FIXIS 
+## USES THE LATEST EXITCODE, WHICH IS FROM GO TEST, AND EXITS WITH THAT. THIS FIXIS 
 # ISSUE #152
 exit $lastexitcode
